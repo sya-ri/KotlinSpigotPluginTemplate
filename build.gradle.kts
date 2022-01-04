99d54d719777746c2c6eb32222941c4d4644f764
sya-ri <sya79lua@gmail.com>
2022-01-04 22:04:27 +0900

    chore: 依存関係のバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ import groovy.lang.Closure
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.0"
-    id("com.github.ben-manes.versions") version "0.39.0"
+    kotlin("jvm") version "1.6.10"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+    id("com.github.ben-manes.versions") version "0.40.0"
     id("com.palantir.git-version") version "0.12.3"
     id("dev.s7a.gradle.minecraft.server") version "1.1.0"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("org.jmailen.kotlinter") version "3.7.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jmailen.kotlinter") version "3.8.0"
 }
 
 val gitVersion: Closure<String> by extra
