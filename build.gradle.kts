65b3ef74169a6bf7ad55c372982b4aa141cdfd61
sya-ri <sya79lua@gmail.com>
2022-01-12 16:41:07 +0900

    chore: 依存関係のバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     kotlin("jvm") version "1.6.10"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
-    id("com.github.ben-manes.versions") version "0.40.0"
+    id("com.github.ben-manes.versions") version "0.41.0"
     id("com.palantir.git-version") version "0.12.3"
     id("dev.s7a.gradle.minecraft.server") version "1.1.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
