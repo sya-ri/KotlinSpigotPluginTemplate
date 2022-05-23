446cbd83c4492cbd96375ba3e619996dda9f216a
sya-ri <git@s7a.dev>
2022-05-23 11:48:40 +0900

    chore: minecraft-server-gradle-plugin を v1.2.0 に更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
     id("com.github.ben-manes.versions") version "0.41.0"
     id("com.palantir.git-version") version "0.12.3"
-    id("dev.s7a.gradle.minecraft.server") version "1.1.0"
+    id("dev.s7a.gradle.minecraft.server") version "1.2.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jmailen.kotlinter") version "3.8.0"
 }
